d62c9e9c0706e3b0295d3bb827e9375ea0508c69
Jay Patel <jaypatel@mac-air-28.cs.iastate.edu>
2017-10-21 19:14:03 -0500

    web crawler kinda done

---
+++ jk.py
@@ -0,0 +1,26 @@
+import urlparse
+import urllib
+from bs4 import BeautifulSoup
+
+url = "http://www.nytimes.com"
+
+urls = ["http://www.nytimes.com", "http://www.cnn.com","http://www.kcci.com"]
+visited = ["http://www.nytimes.com"]
+
+while len (urls) > 0:
+	try:
+		htmltext = urllib.urlopen(urls[0]).read()
+	except:
+		print urls[0]
+	soup = BeautifulSoup(htmltext)
+
+	urls.pop(0)
+	print len(urls)
+
+	for tag in soup.findAll('a', href = True):
+		tag['href'] = urlparse.urljoin(url,tag['href'])
+		if url in tag['href'] and tag['href'] not in visited:
+			urls.append(tag['href'])
+			visited.append(tag['href'])
+
+	print visited
